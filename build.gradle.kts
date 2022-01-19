0291c1da170c4d2a28b16ee59a5a2b0d607d6a56
LaoLittle <y1941803875@outlook.com>
2022-01-19 20:05:06 +0800

    update implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,10 +15,16 @@ repositories {
 }
 
 dependencies {
-    val exposedVersion = "0.37.1"
+    val exposedVersion = "0.37.2"
+    val kumoVersion = "1.28"
+    implementation("org.quartz-scheduler:quartz:2.3.2")
     implementation("org.xerial:sqlite-jdbc:3.36.0.3")
     implementation("com.alibaba:druid:1.2.8")
+    implementation("com.kennycason:kumo-core:$kumoVersion")
+    implementation("com.kennycason:kumo-tokenizers:$kumoVersion")
     implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-java-time:$exposedVersion")
+    implementation(fileTree(mapOf("dir" to "lib", "include" to listOf("*.jar"))))
 }
